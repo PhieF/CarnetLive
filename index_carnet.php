a246165d865b89b9a3f6055c12d1e5e5fd3d522a
Phie <phie@phie.ovh>
2018-09-18 13:59:48 +0200

    fix auto login with cookie

--- index_carnet.php
+++ index_carnet.php
@@ -1,7 +1,7 @@
 <?php
 
 require_once getcwd() . '/lib/base.php';
-if(\OC::$server->getUserSession()->isLoggedIn())
+if(\OC::$server->getUserSession()->isLoggedIn() || !empty($_COOKIE['nc_username']))
 	header("Location: index.php");
 else
 	header("Location: intro/index.html");
